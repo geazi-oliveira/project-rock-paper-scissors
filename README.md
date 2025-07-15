587e9a7e56cadc17cf616a566186466bc6659844
Geazi Oliveira <geazi.assis12345@gmail.com>
2025-07-15 13:28:35 -0300

    add interface

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Rock Paper Scissors Game
 This challenge is part of The Odin Project curriculum. The goal was to create a Rock, Paper, Scissors game that runs entirely in the console. The computer's choice is generated randomly, and the user's option is collected via input.
 
+Now with an interface, the result no longer appears in the console.
+
 The course discourages the use of AI for problem-solving. This code reflects my current understanding, whether it is correct or not.
 
 Challenge Link: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors#project-solution
@@ -12,6 +14,8 @@ Descrição (PT-BR)
 
 Jogo Pedra Papel Tesoura 
 Este desafio faz parte do curso The Odin Project. O objetivo era fazer o jogo do pedra, papel e tesoura para rodar inteiramente no console, gerando aleatoriamente a escolha do computador e coletando por meio de um input a opção do usuário.
+
+Agora com interface, o resultado não aparece mais no console.
 
 O curso desencoraja o uso de IA para resolução de problemas. O código reflete o meu conhecimento atual, seja ele correto ou não.
 
--- script.js
+++ script.js
@@ -1,82 +1,126 @@
-function playGame() {
-   
-    let humanScore = 0;
-    let computerScore = 0;
-    let tie = 0;
-    for (i=1; i <= 5; i++) {
-        let computerChoiceOk = getComputerChoice();
-        let choiceOk = getHumanChoice();
-        console.log(`Round ${i}`)
-            
-        function getComputerChoice() {
+const main = document.querySelector(".container");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const container = document.createElement("div");
+const human = document.createElement("p");
+const computer = document.createElement("p");
+const tie = document.createElement("p");
+const humanChoiceComputerChoice = document.createElement("p");
+const result = document.querySelector(".container");
+let textComputerScore = "Computer score = ";
+let textHumanScore = "Human score = "
+let textTie = "Ties in the math = " ;
+
+container.appendChild(human);
+container.appendChild(computer);
+container.appendChild(tie);
+main.appendChild(container);
+computer.textContent = textComputerScore + 0;
+human.textContent = textHumanScore + 0; 
+tie.textContent = textTie + 0;
+const buttonOptions = document.querySelectorAll(".buttons > button")
+let humanScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+function getComputerChoice() {
             const options = ['rock', 'paper', 'scissors'];
-            let randomOption = Math.floor(Math.random()* options.length)
+            let randomOption = Math.floor(Math.random() * options.length)
             let result = options[randomOption]
             return result;
-        } //This function generates the computer's choice
-                                      
-    
-        function getHumanChoice() {
-            let choice = prompt('Choice rock, paper ou scissors!'); 
-            return choice.toLowerCase();
-        } //Gathers the user's input
-    
-        function playRound(getComputerChoice, getHumanChoice) {
-            switch (getComputerChoice) {
-                case 'paper':  
-                    if(getHumanChoice == 'paper') {
-                        console.log('It\'s a tie! Paper ties with paper!')
-                        ++tie;
-                    } else if (getHumanChoice == 'rock') {
-                        console.log('You lost! Paper beats rock!')
-                        return ++computerScore;
-                    } else if (getHumanChoice == 'scissors') {
-                        console.log('You won! Scissors beats paper!')
-                        return ++humanScore;
-                    }
-                break;
-                case 'rock': 
-                    if(getHumanChoice == 'paper') {
-                        console.log('You won! Paper beats rock!')
-                        ++humanScore;
-                    
-                    } else if (getHumanChoice == 'scissors') {
-                        console.log('You lost! Rock beats scissors!')
-                        ++computerScore;
-                    } else if (getHumanChoice == 'rock') {
-                        console.log('Rock ties with rock!')
-                        ++tie;
-                    }
-                break;
-                case 'scissors': 
-                    if(getHumanChoice == 'paper') {
-                        console.log('You lost! Scissors beats paper!')
-                        ++computerScore;
-                        } else if (getHumanChoice == 'scissors') {
-                        console.log('Scissors ties with scissors.')
-                        ++tie;
-                        } else if (getHumanChoice == 'rock') {
-                        console.log('You won! Rock beats scissors.')
-                        ++humanScore;
-                        }
-                break;   
+        }
+
+function getHumanChoice() {
+        return button.textContent.toLowerCase();
+        
+        }
+
+function playRound(getComputerChoice, getHumanChoice) {
+    humanChoiceComputerChoice.textContent = `Computer: ${getComputerChoice} ------X------ Human: ${getHumanChoice}`
+    result.appendChild(humanChoiceComputerChoice);
+    switch (getComputerChoice) {
+        case 'paper':  
+            if(getHumanChoice == 'paper') {
+                ++tieScore;
+                tie.textContent = textTie + tieScore;
+            } else if (getHumanChoice == 'rock') {
+                ++computerScore;
+                computer.textContent = textComputerScore + computerScore;
+            } else if (getHumanChoice == 'scissors') {
+                ++humanScore;
+                human.textContent = textHumanScore + humanScore;
             }
-        }  //Check for the winner
-        playRound(computerChoiceOk, choiceOk);
+        break;
+        case 'rock': 
+            if(getHumanChoice == 'paper') {
+                ++humanScore;
+                human.textContent = textHumanScore + humanScore;
+            
+            } else if (getHumanChoice == 'scissors') {
+                ++computerScore;
+                computer.textContent = textComputerScore + computerScore;
+            } else if (getHumanChoice == 'rock') {
+                ++tieScore;
+                tie.textContent = textTie + tieScore;
+            }
+        break;
+        case 'scissors': 
+            if(getHumanChoice == 'paper') {
+                ++computerScore;
+                computer.textContent = textComputerScore + computerScore;
+                } else if (getHumanChoice == 'scissors') {
+                ++tieScore;
+                tie.textContent = textTie + tieScore;
+                } else if (getHumanChoice == 'rock') {
+                ++humanScore;
+                human.textContent = textHumanScore + humanScore;
+                }
+        break;   
     }
-    console.log('Result:')
-    if(humanScore > computerScore) { //Announce the winner
-        console.log(`You won ${humanScore} - ${computerScore}`);
-    } else if (computerScore > humanScore) {
-        console.log(`You lost ${computerScore} - ${humanScore}`);
-    } else  {
-        console.log(`This game was a tie ${computerScore} - ${humanScore}`)
-    }
-    
-    if(tie > 0) { // Announce the draws
-        console.log(`This match had ${tie} tie.`)
-    } 
 }
+
+buttonOptions.forEach((button) => {
     
-playGame();
-    
+    button.addEventListener("click", () => { 
+        
+        let computerChoiceOk = getComputerChoice();
+        let humanChoiceOk = button.textContent.toLocaleLowerCase();
+        
+          
+        playRound(computerChoiceOk, humanChoiceOk);
+        
+        if(humanScore === 5) {
+            human.textContent = `Human score = 5. Congratulations, you won this one!`;
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+            rock.style.backgroundColor = "#E2DED2";
+            paper.style.backgroundColor = "#E2DED2";
+            scissors.style.backgroundColor = "#E2DED2";
+            const btnReset = document.createElement("button");
+            btnReset.id = "reset";
+            btnReset.textContent = "Reset";
+            container.appendChild(btnReset);
+             btnReset.addEventListener("click", () => {
+               window.location.reload();
+            });
+            
+        } else if (computerScore === 5) {
+            computer.textContent = `Computer score = 5. Unfortunately, you lost this one!`;
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+            rock.style.backgroundColor = "#E2DED2";
+            paper.style.backgroundColor = "#E2DED2";
+            scissors.style.backgroundColor = "#E2DED2";
+            const btnReset = document.createElement("button");
+            btnReset.id = "reset";
+            btnReset.textContent = "Reset";
+            container.appendChild(btnReset);
+            btnReset.addEventListener("click", () => {
+                window.location.reload();
+            });
+        }    
+    })
+});
